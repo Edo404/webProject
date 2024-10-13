8010177fd95fdb156e16a5316697a3c92d35086b
Edo404 <edoardogamurrini@gmail.com>
2024-10-13 18:26:08 +0200

    My website

---
+++ src/Card.tsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+                <img src="" alt="" />
+                <h2>Edoardo</h2>
+                <p>Developer and Drummer</p>
+        </div>
+    );
+}
+
+export default Card
